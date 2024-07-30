494e46dd830497253040c5c5236feea9301cd61a
caramel0923 <ac.morokuma@gmail.com>
2024-07-30 18:55:40 +0900

    second commit

---
+++ js/script.js
@@ -0,0 +1,296 @@
+pref_jpn = {
+    1: "北海道",
+    2: "青森県",
+    3: "岩手県",
+    4: "宮城県",
+    5: "秋田県",
+    6: "山形県",
+    7: "福島県",
+    8: "茨城県",
+    9: "栃木県",
+    10: "群馬県",
+    11: "埼玉県",
+    12: "千葉県",
+    13: "東京都",
+    14: "神奈川県",
+    15: "新潟県",
+    16: "富山県",
+    17: "石川県",
+    18: "福井県",
+    19: "山梨県",
+    20: "長野県",
+    21: "岐阜県",
+    22: "静岡県",
+    23: "愛知県",
+    24: "三重県",
+    25: "滋賀県",
+    26: "京都府",
+    27: "大阪府",
+    28: "兵庫県",
+    29: "奈良県",
+    30: "和歌山県",
+    31: "鳥取県",
+    32: "島根県",
+    33: "岡山県",
+    34: "広島県",
+    35: "山口県",
+    36: "徳島県",
+    37: "香川県",
+    38: "愛媛県",
+    39: "高知県",
+    40: "福岡県",
+    41: "佐賀県",
+    42: "長崎県",
+    43: "熊本県",
+    44: "大分県",
+    45: "宮崎県",
+    46: "鹿児島県",
+    47: "沖縄県"
+}
+
+const pref_en = {
+    1:"hokkaido",
+    2:"aomori",
+    3:"iwate",
+    4:"miyagi",
+    5:"akita",
+    6:"yamagata",
+    7:"fukushima",
+    8:"ibaraki",
+    9:"tochigi",
+    10:"gunma",
+    11:"saitama",
+    12:"chiba",
+    13:"tokyo",
+    14:"kanagawa",
+    15:"niigata",
+    16:"toyama",
+    17:"ishikawa",
+    18:"fukui",
+    19:"yamanashi",
+    20:"nagano",
+    21:"gifu",
+    22:"shizuoka",
+    23:"aichi",
+    24:"mie",
+    25:"shiga",
+    26:"kyoto",
+    27:"osaka",
+    28:"hyogo",
+    29:"nara",
+    30:"wakayama",
+    31:"tottori",
+    32:"shimane",
+    33:"okayama",
+    34:"hiroshima",
+    35:"yamaguchi",
+    36:"tokushima",
+    37:"kagawa",
+    38:"ehime",
+    39:"kochi",
+    40:"fukuoka",
+    41:"saga",
+    42:"nagasaki",
+    43:"kumamoto",
+    44:"oita",
+    45:"miyazaki",
+    46:"kagoshima",
+    47:"okinawa"
+}
+/*const hokkaido = ["定山渓温泉", "湯の川温泉", "朝里川温泉", "登別温泉", "洞爺湖温泉", "層雲峡温泉", "御園湯温泉", "ウトロ温泉", "川湯温泉", "十津川温泉", "豊富温泉"];
+const aomori = ["浅虫温泉", "酸ヶ湯温泉", "薬研温泉", "下風呂温泉", "大鰐温泉", "蔦温泉", "黄金崎不老ふ死温泉"];
+const iwate = ["繋温泉", "鶯宿温泉", "花巻温泉", "志戸平温泉", "夏油温泉"];
+const miyagi = ["秋保温泉", "作並温泉", "青根温泉", "鳴子温泉", "遠刈田温泉"];
+const akita = ["鶴の湯温泉（乳頭温泉郷）", "黒湯温泉（乳頭温泉郷）", "湯瀬温泉", "玉川温泉（八幡平温泉郷）", "後生掛温泉（八幡平温泉）"];
+const yamagata = ["あつみ温泉", "湯田川温泉", "湯野浜温泉", "白布温泉", "小野川温泉", "赤湯温泉", "天童温泉", "上山温泉", "銀山温泉"];
+const fukushima = ["芦ノ牧温泉", "東山温泉", "飯坂温泉", "土湯温泉", "磐梯熱海温泉", "いわき湯本温泉", "岳温泉"];
+const ibaraki = ["袋田温泉"];
+const tochigi = ["那須湯本温泉", "塩原温泉郷", "湯西川温泉", "川俣温泉", "川治温泉", "鬼怒川温泉"];
+const gunma = ["谷川温泉", "猿ヶ京温泉", "四万温泉", "草津温泉", "老神温泉", "万座温泉", "水上温泉", "伊香保温泉", "川原湯温泉"];
+const saitama = [];
+const chiba = [];
+const tokyo = [];
+const kanagawa = ["湯河原温泉", "箱根温泉郷"];
+const niigata = ["岩室温泉", "弥彦温泉", "月岡温泉", "瀬波温泉", "赤倉温泉", "越後湯沢温泉"];
+const toyama = ["宇奈月温泉"];
+const ishikawa = ["湯涌温泉", "粟津温泉", "和倉温泉", "山中温泉", "山代温泉", "片山津温泉"];
+const fukui = ["芦原温泉"];
+const yamanashi = ["湯村温泉", "石和温泉", "下部温泉"];
+const nagano = ["浅間温泉", "白骨温泉", "鹿教湯温泉", "湯田中温泉", "渋温泉", "野沢温泉", "大町温泉", "昼神温泉", "発哺温泉", "下諏訪温泉"];
+const gifu = ["下呂温泉", "濁河温泉"];
+const shizuoka = ["寸又峡温泉", "舘山寺温泉", "熱海温泉", "伊東温泉", "稲取温泉", "熱川温泉", "修善寺温泉", "湯ノ島温泉", "土肥温泉", "伊豆長岡温泉", "下田温泉", "蓮台寺温泉"];
+const aichi = ["三谷温泉", "西浦温泉"];
+const mie = ["湯の山温泉", "長島温泉"];
+const shiga = ["雄琴温泉"];
+const kyoto = [];
+const osaka = [];
+const hyogo = ["城崎温泉","湯村温泉"];
+const nara = ["十津川温泉"];
+const wakayama = ["湯の峰温泉", "龍神温泉"];
+const tottori = ["吉岡温泉", "浜村温泉", "関金温泉", "三朝温泉", "皆生温泉", "東郷温泉", "はわい温泉", "浜村温泉"];
+const shimane = ["玉造温泉", "三瓶温泉", "温泉津温泉"];
+const okayama = ["奥津温泉（美作三湯）", "湯郷温泉（美作三湯）", "湯原温泉（美作三湯）"];
+const hiroshima = ["湯来温泉"];
+const yamaguchi = ["川棚温泉", "長門湯本温泉", "湯田温泉"];
+const tokushima = [];
+const kagawa = [];
+const ehime = ["道後温泉", "鈍川温泉"];
+const kochi = [];
+const fukuoka = ["原鶴温泉"];
+const saga = ["嬉野温泉", "武雄温泉"];
+const nagasaki = ["雲仙温泉", "小浜温泉", "島原温泉"];
+const kumamoto = ["杖立温泉", "山鹿温泉", "内牧温泉", "菊池温泉", "黒川温泉", "人吉温泉", "日奈久温泉"];
+const oita = ["由布院温泉", "別府温泉", "鉄輪温泉", "天ヶ瀬温泉", "筋湯温泉", "日田温泉"];
+const miyazaki = [];
+const kagoshima = ["古里温泉", "指宿温泉", "霧島温泉郷"];
+const okinawa = [];*/
+onsen_list = [
+    ["定山渓温泉", "湯の川温泉", "朝里川温泉", "登別温泉", "洞爺湖温泉", "層雲峡温泉", "御園湯温泉", "ウトロ温泉", "川湯温泉", "十津川温泉", "豊富温泉"],
+    ["浅虫温泉", "酸ヶ湯温泉", "薬研温泉", "下風呂温泉", "大鰐温泉", "蔦温泉", "黄金崎不老ふ死温泉"],
+    ["繋温泉", "鶯宿温泉", "花巻温泉", "志戸平温泉", "夏油温泉"],
+    ["秋保温泉", "作並温泉", "青根温泉", "鳴子温泉", "遠刈田温泉"],
+    ["鶴の湯温泉（乳頭温泉郷）", "黒湯温泉（乳頭温泉郷）", "湯瀬温泉", "玉川温泉（八幡平温泉郷）", "後生掛温泉（八幡平温泉）"],
+    ["あつみ温泉", "湯田川温泉", "湯野浜温泉", "白布温泉", "小野川温泉", "赤湯温泉", "天童温泉", "上山温泉", "銀山温泉"],
+    ["芦ノ牧温泉", "東山温泉", "飯坂温泉", "土湯温泉", "磐梯熱海温泉", "いわき湯本温泉", "岳温泉"],
+    ["袋田温泉"],
+    ["那須湯本温泉", "塩原温泉郷", "湯西川温泉", "川俣温泉", "川治温泉", "鬼怒川温泉"],
+    ["谷川温泉", "猿ヶ京温泉", "四万温泉", "草津温泉", "老神温泉", "万座温泉", "水上温泉", "伊香保温泉", "川原湯温泉"],
+    [],
+    [],
+    [],
+    ["湯河原温泉", "箱根温泉郷"],
+    ["岩室温泉", "弥彦温泉", "月岡温泉", "瀬波温泉", "赤倉温泉", "越後湯沢温泉"],
+    ["宇奈月温泉"],
+    ["湯涌温泉", "粟津温泉", "和倉温泉", "山中温泉", "山代温泉", "片山津温泉"],
+    ["芦原温泉"],
+    ["湯村温泉", "石和温泉", "下部温泉"],
+    ["浅間温泉", "白骨温泉", "鹿教湯温泉", "湯田中温泉", "渋温泉", "野沢温泉", "大町温泉", "昼神温泉", "発哺温泉", "下諏訪温泉"],
+    ["下呂温泉", "濁河温泉"],
+    ["寸又峡温泉", "舘山寺温泉", "熱海温泉", "伊東温泉", "稲取温泉", "熱川温泉", "修善寺温泉", "湯ノ島温泉", "土肥温泉", "伊豆長岡温泉", "下田温泉", "蓮台寺温泉"],
+    ["三谷温泉", "西浦温泉"],
+    ["湯の山温泉", "長島温泉"],
+    ["雄琴温泉"],
+    [],
+    [],
+    ["城崎温泉","湯村温泉"],
+    ["十津川温泉"],
+    ["湯の峰温泉", "龍神温泉"],
+    ["吉岡温泉", "浜村温泉", "関金温泉", "三朝温泉", "皆生温泉", "東郷温泉", "はわい温泉", "浜村温泉"],
+    ["玉造温泉", "三瓶温泉", "温泉津温泉"],
+    ["奥津温泉（美作三湯）", "湯郷温泉（美作三湯）", "湯原温泉（美作三湯）"],
+    ["湯来温泉"],
+    ["川棚温泉", "長門湯本温泉", "湯田温泉"],
+    [],
+    [],
+    ["道後温泉", "鈍川温泉"],
+    [],
+    ["原鶴温泉"],
+    ["嬉野温泉", "武雄温泉"],
+    ["雲仙温泉", "小浜温泉", "島原温泉"],
+    ["杖立温泉", "山鹿温泉", "内牧温泉", "菊池温泉", "黒川温泉", "人吉温泉", "日奈久温泉"],
+    ["由布院温泉", "別府温泉", "鉄輪温泉", "天ヶ瀬温泉", "筋湯温泉", "日田温泉"],
+    [],
+    ["古里温泉", "指宿温泉", "霧島温泉郷"],
+    []
+];
+
+let Question_cnt = 0;
+let answer_cnt = 0;
+
+const min_pref = 1;
+const max_pref = 47;
+let correct_pref;
+
+//カウント設定
+function Count(){
+    document.getElementById("Question_cnt").innerText = "問題数：" + Question_cnt + "問";
+    document.getElementById("answer_cnt").innerText = "正解数：" + answer_cnt + "問";
+    if (Question_cnt != 0){
+        document.getElementById("answer_rate").innerText = "正解率：" + Math.round(answer_cnt / Question_cnt * 1000) / 10 + "%";
+    }else{
+        document.getElementById("answer_rate").innerText = "正解率：" + 0.00 +"%";
+    }
+}
+
+//問題設定
+function Setup(){
+    //カウント表示
+    Count();
+
+    //正誤表示を非表示
+    document.getElementById("popup").style.display = 'none'; 
+
+    //道県選択
+    do{
+        correct_pref = Random(min_pref, max_pref);
+    }while(onsen_list[correct_pref-1].length == 0);
+
+    //出題問題選択
+    const min_onsen = 0;
+    let correct_onsen = Random(min_onsen, onsen_list[correct_pref-1].length);
+
+    //不正解選択肢選出
+    do{
+        var miss_1 = Random(min_pref, max_pref);
+    }while(correct_pref == miss_1);
+
+    do{
+        var miss_2 = Random(min_pref, max_pref);
+    }while(correct_pref == miss_2  |  miss_2 == miss_1);
+    
+    do{
+        var miss_3 = Random(min_pref, max_pref);
+    }while(correct_pref == miss_3  |  miss_3 == miss_1  ||  miss_3 == miss_2);
+    
+    //選択肢リスト
+    option_list = [correct_pref, miss_1, miss_2, miss_3];
+    
+    //console.log("シャッフル前");
+    //console.log(option_list);
+    
+    //Fisher-Yates shuffle
+    for(var i = option_list.length - 1; i > 0; i--){
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = option_list[i];
+        option_list[i] = option_list[j];
+        option_list[j] = tmp;
+    }
+    //console.log("シャッフル後");
+    //console.log(option_list);
+    
+    //問題文設定
+    document.getElementById("Question").innerText = onsen_list[correct_pref-1][correct_onsen];
+    //選択肢設定
+    document.getElementById("option_1").innerText = pref_jpn[Number(option_list[0])];
+    document.getElementById("option_2").innerText = pref_jpn[Number(option_list[1])];
+    document.getElementById("option_3").innerText = pref_jpn[Number(option_list[2])];
+    document.getElementById("option_4").innerText = pref_jpn[Number(option_list[3])];
+}
+
+function Ans(answer){
+    Question_cnt++;
+    //解答の正誤判定
+    if(option_list[answer-1] == correct_pref){
+        //正解のとき
+        document.getElementById("popup_let").innerText = "正解";
+        document.getElementById("popup_let").style.color = "blue";
+        document.getElementById("popup_img").src = "image/maru.png";
+        answer_cnt++;
+    }else{
+        //不正解のとき
+        document.getElementById("popup_let").innerText = "不正解";
+        document.getElementById("popup_let").style.color = "red";
+        document.getElementById("popup_img").src = "image/batu.png";
+    }
+    document.getElementById("answer").innerText = pref_jpn[correct_pref];
+    document.getElementById("popup").style.display = 'block';
+    setTimeout(Setup, 1500);
+}
+
+//乱数（範囲:min~max 値:整数）
+const Random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+
+window.onload = function(){
+    Setup();
+}
